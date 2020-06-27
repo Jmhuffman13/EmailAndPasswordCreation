847a7ca3a539caf4b9e2dcf2ea31619bd266b23b
juanchoguillo <juancardona0607@gmail.com>
2020-06-27 10:38:55 -0400

    The email and password werification working good

--- PasswordVerificator/Program.cs
+++ PasswordVerificator/Program.cs
@@ -6,38 +6,120 @@ namespace PasswordVerificator
     {
         static void Main(string[] args)
         {
-           
-            
-            Console.WriteLine("Give your email");
-            var userEmail = Console.ReadLine();
 
-            var newUser = new UserCreation(userEmail);
+            var newUser = new UserCreation();
+
+             //Email Validation
             
-            newUser.ValidEmail();
+
+             var addCharacterCounter = 0;
+             var dotCharacterCounter = 0;
+
+             while (addCharacterCounter != 1 || dotCharacterCounter < 1) {
+
+                addCharacterCounter = 0;
+                dotCharacterCounter = 0;
+
+
+                Console.WriteLine("Give me  your email");
+                newUser.UserEmail = Console.ReadLine();
+
+                var userEmail = newUser.UserEmail;
+
+                char[] userEmailArray = userEmail.ToCharArray(); 
+
+                // @ verificator got to have just one 
+                foreach (char character in userEmailArray)
+                {
+                    if (character == '@') { addCharacterCounter = addCharacterCounter + 1; }
+                }
+
+                // "'" dot verificator at least one
+                foreach (char character in userEmailArray)
+                {
+                    if (character == '.') { dotCharacterCounter = dotCharacterCounter + 1; }
+                }
+
+                if (addCharacterCounter == 1 && dotCharacterCounter != 0) { Console.WriteLine("Ok you Got Your Email ok, Now..."); }
+                    else { Console.WriteLine("Your email must be wrong shoud be a miss spelling, So lets try Again!"); }
+            }
+
+
+
+            // PASSWORD VALIDATION
+
+
+            var specialCharacterCounter = 0;
+
+            while (specialCharacterCounter < 2)
+            {
+
+                specialCharacterCounter = 0;
+
+                Console.WriteLine("Create a password");
+                newUser.UserPassword = Console.ReadLine();
+
+                var userPassword = newUser.UserPassword;
+                char[] userPasswordArray = userPassword.ToCharArray();
+
+             
+                // AT least two especial characters verificator
+
+                foreach (char character in userPasswordArray)
+                {
+                    if (character == '@' || character == '!' || character == '#' || character == '$' || character == '%' || character == '&' || character == '*')
+                    { specialCharacterCounter = specialCharacterCounter + 1; }
+                }
+
+                if (specialCharacterCounter > 1) { Console.WriteLine("Ok you Got Your Password ok, Now you are registered!!"); }
+                else { Console.WriteLine("Your password must be incomplete , Remenber you need at least two of this (!@#$%^&*)"); }
+            }
+
+            //newUser.ValidEmail();
+
+            /*while (addCharacterCounter != 1 || dotCharacterCounter < 1)
+            {
+                Console.WriteLine("OOps looks like your email must be wrong , maybe is a miss spelling");
+                Console.WriteLine("So Lets Try again  your email");
+                newUser.UserEmail = Console.ReadLine();
+            }
+
+
+
+         
+
+
+
+
+            // PassWord VAlidation
 
             Console.WriteLine("Now type a strong Password");
             newUser.UserPassword = Console.ReadLine();
             var specialCharacterCounter = 0;
 
             var userPassword = newUser.UserPassword;
+
             char[] userPasswordArray = userPassword.ToCharArray();
-            foreach(var character in userPasswordArray)
+            foreach (var character in userPasswordArray)
             {
-                if(character == '@' || character == '!' || character == '#' || character == '$' || character == '%' || character == '&' || character == '*' )
+                if (character == '@' || character == '!' || character == '#' || character == '$' || character == '%' || character == '&' || character == '*')
                 {
-                    specialCharacterCounter = specialCharacterCounter + 1;
+                    specialCharacterCounter++;
                 }
-               
-               
             }
 
-             
-            while (newUser.UserPassword.Length < 8  ||  specialCharacterCounter > 0)
+            if (newUser.UserPassword.Length < 8) { Console.WriteLine($"Try Again your password dosen't have at least 8 characteres"); }
+            if (specialCharacterCounter == 0) { Console.WriteLine($"Try Again your password dosen't have any special character"); }
+            else { Console.WriteLine("Wonderfull your password have been created"); }
+
+            while (newUser.UserPassword.Length < 8  ||  specialCharacterCounter == 0)
             {
+
                 Console.WriteLine("Sorry Your Password is not strong enougth, Please create a new  Password");
                 Console.WriteLine(" And also remember your password must have at least an special character");
-                newUser.UserPassword = Console.ReadLine();
-            }
+
+
+            }*/
 
 
 
--- PasswordVerificator/UserCreation.cs
+++ PasswordVerificator/UserCreation.cs
@@ -12,10 +12,9 @@ namespace PasswordVerificator
 
 
 
-            public  UserCreation( string userEmail)
+            public  UserCreation()
             {
-            UserEmail = userEmail;
-
+           
             }
 
             public string  UserEmail { get; set; }
@@ -27,35 +26,7 @@ namespace PasswordVerificator
 
             public void ValidEmail()
         {
-            var addCharacterCounter = 0;
-            var dotCharacterCounter = 0;
-            var addVerified = false;
-            var dotVerified = false;
-            var emailVerified = false;
-            var userEmail = UserEmail;
-            char[] userEmailArray = userEmail.ToCharArray();
            
-            // @ verificator got to have just one 
-            foreach (char character in userEmailArray)
-            {
-                if (character == '@') { addCharacterCounter = addCharacterCounter + 1; }
-            }
-            if (addCharacterCounter == 1) { addVerified = true; }
-           
-
-            // "'" dot verificator at least one
-            foreach (char character in userEmailArray)
-            {
-                if (character == '.') { dotCharacterCounter = dotCharacterCounter + 1; }
-            }
-            if (dotCharacterCounter != 0) { dotVerified = true; } 
-            
-            if (dotVerified && addVerified) { emailVerified = true; }
-
-            if (emailVerified) { Console.WriteLine("Welcome"); }
-            else { Console.WriteLine("Your email is wrong, Could be a miss spelling!"); }
-            
-
             
             
         }
