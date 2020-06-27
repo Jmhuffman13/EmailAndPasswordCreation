89d6555583e9e31ced0a69c8550e0e79f5273c01
juanchoguillo <juancardona0607@gmail.com>
2020-06-27 01:20:28 -0400

    Until the mail verification

---
+++ PasswordVerificator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PasswordVerificator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Give your email");
+            var userEmail = Console.ReadLine();
+
+            Console.WriteLine("Give me your Password");
+            var userPassword = Console.ReadLine();
+           
+            
+            var newUser = new UserCreation(userEmail,userPassword );
+            
+            newUser.ValidEmail();
+
+            
+            
+            
+        }
+    }
+}
---
+++ PasswordVerificator/UserCreation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PasswordVerificator
+{
+    class UserCreation
+    {
+
+
+
+
+
+
+            public  UserCreation( string userEmail, string userPassword)
+            {
+            UserEmail = userEmail;
+
+            userPassword = UserPassword;
+
+            }
+
+            public string  UserEmail { get; set; }
+            public string  UserPassword { get; set; }
+
+
+          
+
+            public void ValidEmail()
+        {
+            var addCharacterCounter = 0;
+            var dotCharacterCounter = 0;
+            var addVerified = false;
+            var dotVerified = false;
+            var emailVerified = false;
+            var userEmail = UserEmail;
+            char[] userEmailArray = userEmail.ToCharArray();
+           
+            // @ verificator got to have just one 
+            foreach (char character in userEmailArray)
+            {
+                if (character == '@') { addCharacterCounter = addCharacterCounter + 1; }
+            }
+            if (addCharacterCounter == 1) { addVerified = true; }
+           
+
+            // "'" dot verificator at least one
+            foreach (char character in userEmailArray)
+            {
+                if (character == '.') { dotCharacterCounter = dotCharacterCounter + 1; }
+            }
+            if (dotCharacterCounter != 0) { dotVerified = true; } 
+            
+            if (dotVerified && addVerified) { emailVerified = true; }
+
+            Console.WriteLine($"is the email verified : {emailVerified}");
+            
+        }
+    }
+}
