173526bd73e5c63dd1442ad5c6a1d7e2df9f93ba
juanchoguillo <juancardona0607@gmail.com>
2020-06-29 00:59:53 -0400

    Now is a miniBank

--- PasswordVerificator/Program.cs
+++ PasswordVerificator/Program.cs
@@ -8,19 +8,168 @@ namespace PasswordVerificator
     {
         static void Main(string[] args)
         {
+            var programOn = true;
 
-            var newUser = new UserCreation();
+            var juansBankTransactionList = new List<UserCreation>();  // I wanted to use this List of Object 
+            var juansBankEmailList = new List<string>();
+            var juansBankPasswordList = new List<string>();
+            var juansBankNameList = new List<string>();
+            var juansBankBalanceList = new List<double>(); //Is Not updating the balance in tha manager account 
+            var juansBankMoneyInList = new List<double>();
+
+
+            while (programOn == true)
+            {
+                Console.WriteLine("________________________________________");
+                Console.WriteLine("Welcome to Juan's Bank");
+                Console.WriteLine("Here are your options: Writte the letter that correspond to the option you want");
+                Console.WriteLine("Login - Type L");
+                Console.WriteLine("Sign up - Type S");
+                Console.WriteLine("Type Out  - to Leave the platform");
+                Console.WriteLine("________________________________________");
+
+
+                var userOption = Console.ReadLine().ToLower();
+
+                if (userOption == "l")
+                {
+
+                    if (juansBankEmailList.Count > 0)
+                    {
+                        var userIndex = 0;
+                        var emailMatched = false;
+
+                        while (emailMatched == false)
+                        {
+                            Console.WriteLine("Tell me the email you registered with");
+                            var emailInput = (Console.ReadLine());
+
+                            for (var i = juansBankEmailList.Count; i > 0;  i--)  
+                            {
+                                if (emailInput == juansBankEmailList[i -1])
+                                {
+                                    userIndex = i - 1;
+                                    emailMatched = true;
+
+                                    var passwordVerified = false;
+
+                                    while(passwordVerified == false)
+                                    {
+                                        Console.WriteLine("Introduce your password");
+                                        var passwordInput = (Console.ReadLine());
+
+                                        if(passwordInput == juansBankPasswordList[userIndex])
+                                        {
+                                            Console.WriteLine($"Your account has been verified and your Balance is ${juansBankBalanceList[userIndex]}");
+                                            passwordVerified = true; 
+                                        }
+
+
+                                    }
+
+
+                                }
+                                
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry Your ar not registered yet");
+                    }
+                   
+                   
+                }
+                else if(userOption == "s")
+                {
+                    var newUser = new UserCreation();
+
+                    NameCreator(newUser);
+                    EmailChecker(newUser);
+                    PasswordChecker(newUser);
+
+                    Console.WriteLine("How many are you going to deposit in your Bank Account");
+                    newUser.UserMoneyIn = double.Parse(Console.ReadLine());
+
+                    juansBankTransactionList.Add(newUser);// Wanted to use with the object List
+
+                    juansBankEmailList.Add(newUser.UserEmail);
+                    juansBankPasswordList.Add(newUser.UserPassword);
+                    juansBankNameList.Add(newUser.UserName);
+                    juansBankMoneyInList.Add(newUser.UserMoneyIn);
+                    juansBankBalanceList.Add(newUser.UserBalance + newUser.UserMoneyIn - newUser.UserMoneyOut);
+                    
+                    newUser.BalanceAccount();
+
+                    //juansBankBalanceList.Add(newUser.UserBalance);
+                }
+                else if(userOption == "admin") // Why Can I not use this as a methos appart it says that de list doesn' t exist
+                {
+                    Console.WriteLine("You Have log in as the Manger of the Bank, please type the password to check all the transactions so far");
+
+                    var managerPassword = Console.ReadLine();
+                    if (managerPassword == "admin")
+                    {
+                        if (juansBankEmailList.Count > 0)
+                        {
+                            Console.WriteLine("________________________________________");
+                            Console.WriteLine("This is the List Of the transactions");
+
+                            for(var i = 0; i < juansBankEmailList.Count; i++)
+                            {
+                                Console.WriteLine($"Name:{juansBankNameList[i]} || E-mail : {juansBankEmailList[i]} || Balance : {juansBankBalanceList[i]} ");
+                            }
+                        }
+                        else
+                        {
+                        
+                        Console.WriteLine(" You Dont Have Any Customer Yet");
+                        }
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(" Im sorry The Password For the Manager Account is Wrong, Your Access has been Denied");
+                    }
+
+                }
+                else if(userOption == "out")
+                {
+                    Console.WriteLine("Sorry you have to leave, see you later");
+                    programOn = false;      
+                }
+
+            }
+
+            
+
+
 
            
-            EmailChecker(newUser);
 
-            PasswordChecker(newUser);
+           
+        }
+
+        // Manager Access
+        public static void ManagerAccess()
+        {
+          
 
 
         }
 
 
-        //Email Validation Method
+        // Name Creation Methot
+        public static void NameCreator(UserCreation newUser)
+        {
+            Console.WriteLine("what is your name?");
+            newUser.UserName = Console.ReadLine();
+            
+        }
+
+        //Email Creation and Validation Method
 
         public static void EmailChecker(UserCreation newUser)
         {
@@ -60,7 +209,7 @@ namespace PasswordVerificator
 
         }
 
-        // PassWord VAlidation Method
+        // PassWord Creation and VAlidation Method
         public static void PasswordChecker(UserCreation newUser)
         {
             var specialCharacterCounter = 0;
--- PasswordVerificator/UserCreation.cs
+++ PasswordVerificator/UserCreation.cs
@@ -9,20 +9,27 @@ namespace PasswordVerificator
             public  UserCreation()
             {
            
+
             }
 
+        public void BalanceAccount()
+        {
+            Balance = Balance + UserMoneyIn - UserMoneyOut; // Why It dosent work when i holde it in the Balance List
+
+            Console.WriteLine($"The Balance of {UserName}'s account is : $ {Balance}");
+
+        }
+            public string UserName { get; set; }
             public string  UserEmail { get; set; }
             public string  UserPassword { get; set; }
-            public bool EmailVerified { get; set; }
+            public int UserBalance { get; set; }
+            public double Balance { get; set; }
+            public double UserMoneyIn { get; set; }
+            public double UserMoneyOut { get; set; }
 
 
           
 
-            public void ValidEmail()
-        {
            
-            
-            
-        }
     }
 }
